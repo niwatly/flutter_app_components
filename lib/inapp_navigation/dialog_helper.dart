457ce8be073c985d651d1a899aaede886054e069
niwatly <bgkc28@gmail.com>
2020-06-12 01:24:08 +0900

    fix: DialogHelperのデザイン調整

--- lib/inapp_navigation/dialog_helper.dart
+++ lib/inapp_navigation/dialog_helper.dart
@@ -9,24 +9,24 @@ class DialogRoute<T> extends PopupRoute<T> {
     this.builder,
     this.barrierDismissible = true,
   }) : super(settings: settings);
-
+  
   @override
   final bool barrierDismissible;
-
+  
   @override
   String get barrierLabel => null;
-
+  
   @override
   Color get barrierColor => const Color(0x80000000);
-
+  
   @override
   Duration get transitionDuration => const Duration(milliseconds: 200);
-
+  
   @override
   Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
     return builder(context);
   }
-
+  
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
     return FadeTransition(
@@ -47,7 +47,7 @@ class DialogBuilder {
   final TextStyle okStyle;
   final Widget loadingWidget;
   final TextStyle loadingMessageStyle;
-
+  
   DialogBuilder({
     @required this.okLabel,
     @required this.cancelLabel,
@@ -58,41 +58,41 @@ class DialogBuilder {
     @required this.loadingWidget,
     @required this.loadingMessageStyle,
   });
-
+  
   Route error(String message) => DialogRoute(
-        builder: (context) => _createDialog(
-          context: context,
-          title: errorLabel,
-          body: message,
-          actions: [
-            FlatButton(
-              child: Text(
-                okLabel,
-                style: okStyle,
-              ),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ],
+    builder: (context) => _createDialog(
+      context: context,
+      title: errorLabel,
+      body: message,
+      actions: [
+        FlatButton(
+          child: Text(
+            okLabel,
+            style: okStyle,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-      );
-
+      ],
+    ),
+  );
+  
   Route confirm(String message) => DialogRoute(
-        builder: (context) => _createDialog(
-          context: context,
-          title: confirmLabel,
-          body: message,
-          actions: [
-            FlatButton(
-              child: Text(
-                okLabel,
-                style: okStyle,
-              ),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ],
+    builder: (context) => _createDialog(
+      context: context,
+      title: confirmLabel,
+      body: message,
+      actions: [
+        FlatButton(
+          child: Text(
+            okLabel,
+            style: okStyle,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-      );
-
+      ],
+    ),
+  );
+  
   Route<bool> ask({
     String title,
     String message,
@@ -120,91 +120,84 @@ class DialogBuilder {
           ],
         ),
       );
-
+  
   Route<int> pick(List<String> candidates, {String title, String message}) => DialogRoute<int>(
-        builder: (context) => FloatingDialog(
-          onClose: () => Navigator.of(context).pop(-1),
-          title: title,
-          contentBackgroundColor: DialogTheme.of(context).backgroundColor,
-          child: ListView.separated(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            separatorBuilder: (context, index) => const FractionallySizedBox(
-              widthFactor: 0.8,
-              child: Divider(),
-            ),
-            padding: const EdgeInsets.only(bottom: 16, left: 16, right: 16),
-            itemCount: candidates.length,
-            itemBuilder: (context, index) => InkWell(
-              onTap: () => Navigator.of(context).pop(index),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text(
-                  candidates[index],
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: DialogTheme.of(context).contentTextStyle,
-                ),
-              ),
+    builder: (context) => FloatingDialog(
+      onClose: () => Navigator.of(context).pop(-1),
+      title: title,
+      child: ListView.separated(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        separatorBuilder: (context, index) => const Divider(height: 12),
+        itemCount: candidates.length,
+        itemBuilder: (context, index) => InkWell(
+          onTap: () => Navigator.of(context).pop(index),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              candidates[index],
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 5,
+              style: DialogTheme.of(context).contentTextStyle,
             ),
           ),
         ),
-      );
-
+      ),
+    ),
+  );
+  
   Route<T> loading<T>({
     Future<T> future,
     String message,
   }) {
-    final child = WillPopScope(
-      onWillPop: () async => false,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          loadingWidget,
-          const SizedBox(height: 16),
-          if (message != null && message.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 36),
-              child: Material(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                ),
-                color: const Color(0x60000000),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                  child: Text(
-                    message,
-                    style: loadingMessageStyle,
-                  ),
-                ),
-              ),
-            )
-        ],
-      ),
-    );
-
     return DialogRoute(
       builder: (context) => FutureBuilder(
         future: future,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             T data;
-
+            
             if (snapshot.hasData) {
               data = snapshot.data;
             }
-
+            
             WidgetsBinding.instance.addPostFrameCallback((_) => Navigator.of(context).pop(data));
           }
-
-          return child;
+          
+          return WillPopScope(
+            onWillPop: () async => false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                loadingWidget,
+                const SizedBox(height: 16),
+                if (message != null && message.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 36),
+                    child: Material(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                      color: DialogTheme.of(context).backgroundColor,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                        child: Text(
+                          message,
+                          style: loadingMessageStyle,
+                        ),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          );
         },
       ),
       barrierDismissible: false,
     );
   }
-
+  
   AlertDialog _createDialog({
     BuildContext context,
     String title,
@@ -212,14 +205,10 @@ class DialogBuilder {
     List<Widget> actions,
   }) =>
       AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
         title: title?.isNotEmpty == true ? Text(title) : null,
         content: Text(
           body,
         ),
-        contentTextStyle: Theme.of(context).textTheme.bodyText2.apply(fontSizeFactor: 1),
         actions: actions,
       );
 }
--- lib/inapp_navigation/floating_dialog.dart
+++ lib/inapp_navigation/floating_dialog.dart
@@ -4,24 +4,24 @@ class FloatingDialog extends StatelessWidget {
   final Widget child;
   final double widthFactor;
   final String title;
-  final Color contentBackgroundColor;
   final BorderRadius cornerRadius;
+  final EdgeInsets contentPadding;
   final void Function() onClose;
-
+  
   const FloatingDialog({
     @required this.child,
     this.onClose,
     this.title,
-    this.contentBackgroundColor,
-    this.cornerRadius = const BorderRadius.all(Radius.circular(16)),
+    this.cornerRadius = const BorderRadius.all(Radius.circular(8)),
+    this.contentPadding = const EdgeInsets.all(24),
     this.widthFactor = 0.8,
   });
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0x80000000),
-
+      
       /// 枠外タップでダイアログを消したい
       body: GestureDetector(
         onTap: onClose ?? () => Navigator.of(context).pop(),
@@ -29,44 +29,41 @@ class FloatingDialog extends StatelessWidget {
           /// コンテンツの高さが画面幅を超えるときはスクロールさせたい
           builder: (context, constraints) => SingleChildScrollView(
             physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
-
+            
             /// コンテンツの横幅を0.8倍にして中央に表示したい
             child: Center(
               child: ConstrainedBox(
                 constraints: BoxConstraints(maxWidth: constraints.maxWidth * widthFactor, minHeight: constraints.maxHeight),
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(0, 48, 0, 48),
-
+                  
                   /// コンテンツの縦幅は可変にしつつ、親のサイズを画面いっぱいまで伸ばすことで
                   /// コンテンツの高さによらず中央揃えしたい
                   child: Center(
                     /// コンテンツがクリックされてもダイアログを消したくない（Scaffold直下のGestureDetectorまで伝搬してしまう）
                     child: GestureDetector(
                       onTap: () {},
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: contentBackgroundColor,
-                          borderRadius: cornerRadius,
-                        ),
+                      child: Material(
+                        borderRadius: cornerRadius,
+                        color: DialogTheme.of(context).backgroundColor,
                         child: ClipRRect(
                           borderRadius: cornerRadius,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (title != null)
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: 16),
-                                  child: Text(
+                          child: Padding(
+                            padding: contentPadding,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (title != null) ...[
+                                  Text(
                                     title,
-                                    textAlign: TextAlign.center,
+                                    textAlign: TextAlign.left,
                                     style: DialogTheme.of(context).titleTextStyle,
                                   ),
-                                ),
-                              Material(
-                                child: child,
-                              ),
-                            ],
+                                ],
+                                child,
+                              ],
+                            ),
                           ),
                         ),
                       ),
