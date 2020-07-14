816cac739c4e7a5e6d58c59f09813c1d51f49eb3
niwatly <bgkc28@gmail.com>
2020-07-14 12:19:17 +0900

    fix: check disposed in PaginationIndicator

--- lib/view/paging_indicator.dart
+++ lib/view/paging_indicator.dart
@@ -66,9 +66,17 @@ class PaginationIndicator extends StatelessWidget {
 }
 
 class _Notifier extends ValueNotifier<_State> {
+  bool disposed = false;
+
   _Notifier() : super(_State.Idle);
 
-  bool checkNeedLoad(ScrollNotification notification, bool hasMore) {
+  @override
+  void dispose() {
+    disposed = true;
+    super.dispose();
+  }
+
+  checkNeedLoad(ScrollNotification notification, bool hasMore) {
     final metrics = notification.metrics;
 
 //    logger.info("""
@@ -99,6 +107,10 @@ class _Notifier extends ValueNotifier<_State> {
   }
 
   Future igniteAndWaitUntilFinished(Future Function() onPagination) async {
+    if (disposed) {
+      return;
+    }
+    
     value = _State.Loading;
     notifyListeners();
 
