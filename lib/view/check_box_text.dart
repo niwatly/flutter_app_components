f4e56095966e524cacd33cd4e3faa13fd94f2e97
niwatly <bgkc28@gmail.com>
2020-10-19 14:51:36 +0900

    add check_box_text

---
+++ lib/view/check_box_text.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxText extends StatelessWidget {
+  final String label;
+  final String description;
+  final Widget leading;
+  final Widget secondary;
+  final Function(bool newValue) onChanged;
+  final bool checkBoxValue;
+  final bool disable;
+  final EdgeInsets padding;
+  final ListTileControlAffinity affinity;
+  final bool expandBetweenCheckAndLabel;
+  
+  const CheckBoxText({
+    @required this.label,
+    @required this.onChanged,
+    @required this.checkBoxValue,
+    this.expandBetweenCheckAndLabel = true,
+    this.padding,
+    this.leading,
+    this.secondary,
+    this.description,
+    this.disable = false,
+    this.affinity = ListTileControlAffinity.trailing,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    final checkbox = Checkbox(
+      value: checkBoxValue,
+      onChanged: disable ? null : (v) => onChanged(v),
+    );
+    return InkWell(
+      onTap: () => disable ? null : onChanged(!checkBoxValue),
+      child: Padding(
+        padding: padding ?? const EdgeInsets.all(0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            /// checkbox (if leading)
+            if (affinity == ListTileControlAffinity.leading) checkbox else const SizedBox(width: 16),
+            
+            /// leading
+            if (leading != null) leading,
+            
+            /// label
+            _FlexibleOrExpand(
+              expand: expandBetweenCheckAndLabel,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    label,
+                    maxLines: 2,
+                    textAlign: TextAlign.left,
+                    style: Theme.of(context).textTheme.bodyText2.apply(
+                      color: disable ? Theme.of(context).disabledColor : Theme.of(context).textTheme.bodyText2.color,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  if (description != null)
+                    Text(
+                      description,
+                      maxLines: 5,
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                ],
+              ),
+            ),
+            
+            /// secondary
+            if (secondary != null) secondary,
+            
+            /// checkbox (if trailing)
+            if (affinity == ListTileControlAffinity.trailing) checkbox else const SizedBox(width: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _FlexibleOrExpand extends StatelessWidget {
+  final bool expand;
+  final Widget child;
+  
+  const _FlexibleOrExpand({
+    this.expand,
+    this.child,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    if (expand) {
+      return Expanded(
+        child: child,
+      );
+    } else {
+      return Flexible(
+        child: child,
+      );
+    }
+  }
+}
