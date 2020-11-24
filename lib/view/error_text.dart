08a292e9518edf32a9d6296e9f46c47039df9a05
niwatly <bgkc28@gmail.com>
2020-11-24 16:27:07 +0900

    add error_text

---
+++ lib/view/error_text.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_components/view/refresh_button.dart';
+
+class ErrorText extends StatelessWidget {
+  final String label;
+  final Future Function() retryCallback;
+
+  const ErrorText({
+    this.label,
+    this.retryCallback,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            child: Text(
+              label,
+              maxLines: 2,
+            ),
+          ),
+          if (retryCallback != null)
+            RefreshButton(
+              onRefresh: retryCallback,
+            ),
+        ],
+      ),
+    );
+  }
+}
