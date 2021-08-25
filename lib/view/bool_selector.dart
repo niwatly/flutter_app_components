fa385da8cbd07c80e01251ba5c637c8c8a65f2a3
niwatly <bgkc28@gmail.com>
2021-08-25 20:11:07 +0900

    add bool selector 2

--- lib/view/bool_selector.dart
+++ lib/view/bool_selector.dart
@@ -14,3 +14,17 @@ class BoolSelector<T> extends Selector<T, bool> {
           selector: (context, x) => selector(context, x),
         );
 }
+
+class BoolSelector2<T, S> extends Selector2<T, S, bool> {
+  BoolSelector2({
+    Key? key,
+    required bool Function(BuildContext, T, S) selector,
+    required Widget Function(BuildContext) builder,
+    ShouldRebuild<bool>? shouldRebuild,
+  }) : super(
+          key: key,
+          shouldRebuild: shouldRebuild,
+          builder: (context, x, child) => x ? builder(context) : SizedBox.shrink(),
+          selector: (context, t, s) => selector(context, t, s),
+        );
+}
