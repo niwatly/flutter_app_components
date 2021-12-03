06632dd5903db9f32614a954e06e10ceba16e591
Kota Hayashi <geukfyger@gmail.com>
2021-12-03 16:32:31 +0900

    Add: FlutterバージョンとPackageバージョンを最新化

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -76,6 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+        // ignore: avoid-non-null-assertion
         title: Text(widget.title!),
       ),
       body: Center(
